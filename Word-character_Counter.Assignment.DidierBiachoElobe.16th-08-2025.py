d3bed406215a7feb3f5be654daf53b6cc17b7592
Didier511 <diddier79@icloud.com>
2026-05-22 02:38:33 +0000

    Update Word-character_Counter.Assignment.DidierBiachoElobe.16th-08-2025.py

--- Word-character_Counter.Assignment.DidierBiachoElobe.16th-08-2025.py
+++ Word-character_Counter.Assignment.DidierBiachoElobe.16th-08-2025.py
@@ -2,16 +2,17 @@
 # 16th August 2025 
 # Assignment 
 
-print("\nThis program counts the number of words, characters and digits in a text!\n")
+print("\nThis program counts the number of words, characters and digits in a text!\n"+"="*73+"\n") 
+
 def word_counter(text):
     words=len(text.split())
-    letters=len(text)
+    characters=len(text)
     dot=text.count(".")
     comma=text.count(",")
     excl_mark=text.count("!")
     intr_mark=text.count("?")
 
-    #other characters
+    #special characters
     asterisk=text.count("*")
     aroba=text.count("@")
     equal_to=text.count("=")
@@ -33,35 +34,71 @@ def word_counter(text):
     number0=text.count("0")
     
 
-    numbers=number1+number2+number3+number4+number5+number6+number7+number8+number9+number0
-    characters=dot+comma+excl_mark+intr_mark
-    other_characters=asterisk+aroba+equal_to+minus+plus+dollar_sgn+ampersand
+    numbers = number1 + number2 + number3 + number4 + number5 + number6 + number7 + number8 + number9 + number0 
+    
+    punct_mark = dot + comma + excl_mark + intr_mark
 
-    return letters, words, characters, dot,comma, excl_mark, intr_mark,other_characters, numbers
+    special_characters = asterisk + aroba + equal_to + minus + plus + dollar_sgn + ampersand
+
+    return characters, words, punct_mark, dot,comma, excl_mark, intr_mark,special_characters, numbers
 
 if __name__=="__main__":
 
-    Utext=input("Enter your text: ")
+    UserText=input("Enter your text: ")
 
-    letters, words, characters, dot, comma, excl_mark, intr_mark,other_characters, numbers= word_counter(Utext)
+    characters, words, punct_mark, dot, comma, excl_mark, intr_mark,special_characters, numbers= word_counter(UserText)
+
     print("\nCount Result:\n")
-    print(f"There is/are: {words} word/s.\n")
-    print(f"There is/are: {letters} letter/s.\n")
-    print(f"There is/are: {numbers} digit/s.\n")
-    print(f"There is/are {characters} character/s:")
-    print(f"{dot} dot/s.")
-    print(f"{comma} comma/s.")
-    print(f"{excl_mark} exclamation mark/s.")
-    print(f"{intr_mark} interrogation mark/s.\n")
-    print(f"There is/are: {other_characters} other characters/s.\n")
+    if words > 1:
+        print(f"There are: {words} words.\n")
+    else: 
+        print(f"There is: {words} word.\n")
+        
+    if characters > 1: 
+        print(f"There are: {characters} characters.\n")
+    else:
+        print(f"There is: {characters} character.\n")
 
-    print("-"*41,"\n\nTHANK YOU!\n")
-
+    if numbers > 1: 
+        print(f"There are: {numbers} digits.\n")
+    else:
+        print(f"There is: {numbers} digit.\n")
     
-    
-    
+    if punct_mark > 1: 
+        print(f"There are {punct_mark} punctuation marks:")
+    else: 
+        print(f"There is {punct_mark} punctuation mark:")
+
+    if dot > 1: 
+        print(f"{dot} dots.")
+    else: 
+        print(f"{dot} dot.")
+
+    if comma > 1: 
+        print(f"{comma} commas.")
+    else: 
+        print(f"{comma} comma.")
+
+    if excl_mark > 1: 
+        print(f"{excl_mark} exclamation marks.")
+    else: 
+        print(f"{excl_mark} exclamation mark.")
+
+    if intr_mark > 1:     
+        print(f"{intr_mark} interrogation marks.\n")
+    else: 
+        print(f"{intr_mark} interrogation mark.\n")
+
+    if special_characters > 1: 
+        print(f"There are: {special_characters} special characters.\n")
+    else:
+        print(f"There is: {special_characters} special character.\n")
 
 
-word_counter(Utext)
+    print("-"*41,"\n\nTHANK YOU!\n\n"+"-"*41)
+
+
+
+word_counter(UserText)
 
 
